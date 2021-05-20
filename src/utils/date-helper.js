28ea64eeffe27a52956aba8a2b12b687500f97da
Ryanksa <ryanhungjen.lin@mail.utoronto.ca>
2021-05-20 13:32:57 -0400

    Added scrollbar to DateIcon hover. Updated numDaysBetween fix

--- src/utils/date-helper.js
+++ src/utils/date-helper.js
@@ -38,11 +38,13 @@ export const formatUpcomingDueDate = (time) => {
 // returns 0 if d1 and d2 are on the same day
 // else returns number of days from d1 to d2 (negative if d1 is passed d2)
 export const numDaysBetween = (d1, d2) => {
-    const numDays = Math.floor((d2.getTime() - d1.getTime()) / MILLISECSPERDAY);
+    const numDays = Math.round((d2.getTime() - d1.getTime()) / MILLISECSPERDAY);
     
-    // this deals with the problem of timezones/daylight saving time messing up the calculation
+    // this deals with the problem of getTime() using UTC instead of local timezone
     if (numDays === 0 && d1.getDay() !== d2.getDay()) {
-        return 1;
+        return d1.getTime() < d2.getTime() ? 1 : -1;
+    } else if ((numDays === 1 || numDays === -1) && d1.getDay() === d2.getDay()) {
+        return 0;
     }
     
     return numDays;
