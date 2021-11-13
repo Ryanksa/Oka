6c110fe59831fb2ce97a35e63c6e9b6edbaaa4f8
Ryanksa <ryanlin58@gmail.com>
2021-11-13 18:55:44 -0500

    Added scene content to Take A Break page

--- src/components/TakeABreak/TakeABreak.jsx
+++ src/components/TakeABreak/TakeABreak.jsx
@@ -4,8 +4,31 @@ import './TakeABreak.scss'
 class TakeABreak extends Component {
     render() { 
         return (
-            <div className="break-container">
-                Under Construction...
+            <div class="scene">
+                <div class="sun-container">
+                    <div class="sun"></div>
+                </div>
+                <div class="mountain-1"></div>
+                <div class="mountain-2"></div>
+                <div class="mountain-3"></div>
+                <div class="grass-1"></div>
+                <div class="grass-2"></div>
+                <div class="grass-3"></div>
+                <div class="grass-4"></div>
+                <div class="rock-1 rock-reflection"></div>
+                <div class="rock-2 rock-reflection"></div>
+                <div class="rock-3 rock-reflection"></div>
+                <div class="rock-4 rock-reflection"></div>
+                <div class="rock-5 rock-reflection"></div>
+                <div class="rock-6 rock-reflection"></div>
+                <div class="rock-7 rock-reflection"></div>
+                <div class="rock-8 rock-reflection"></div>
+                <div class="rock-9 rock-reflection"></div>
+                <div class="rock-10 rock-reflection"></div>
+                <div class="water"></div>
+                <div class="smoke-1"></div>
+                <div class="smoke-2"></div>
+                <div class="smoke-3"></div>
             </div>
         );
     }
