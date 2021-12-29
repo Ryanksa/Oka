0f71a4f6136bef59e6695c224aef753c44d68d5f
Ryanksa <61597505+Ryanksa@users.noreply.github.com>
2021-12-28 21:10:23 -0500

    Added news and weather voice commands (#14)

--- components/Assistant.tsx
+++ components/Assistant.tsx
@@ -9,9 +9,14 @@ import {
 import { AssistantWithUrl } from "../models/assistant";
 import defaultAssistant from "../assets/default-assistant.svg";
 
-import Image from "next/image";
-
 import * as SpeechRecognizer from "../utils/speech-recognizer";
+import { getIpInfo } from "../utils/ip-service";
+import { getTopHeadlines } from "../utils/news-service";
+import { getWeatherOneCall } from "../utils/weather-service";
+import { News } from "../models/news";
+import { CurrentWeather } from "../models/weather";
+
+import Image from "next/image";
 
 const Assistant = () => {
   const assistantContext = useContext(AssistantContext);
@@ -21,7 +26,7 @@ const Assistant = () => {
     avatar: assistantContext.avatar,
     avatarUrl: assistantContext.avatarUrl,
   });
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState<string | JSX.Element>("");
 
   useEffect(() => {
     const callback = () => {
@@ -42,10 +47,65 @@ const Assistant = () => {
       SpeechRecognizer.addCommand({
         prompt: "google *",
         callback: (query: string) => {
-          showMessage(`Googling "${query}"`);
+          showMessage(`Googling "${query}"`, 5000);
           window.open("https://www.google.com/search?q=" + query);
         },
       });
+
+      SpeechRecognizer.addCommand({
+        prompt: "what's on the news",
+        callback: () => {
+          getIpInfo()
+            .then((res) => {
+              return getTopHeadlines(res.data.country);
+            })
+            .then((res) => {
+              if (res.data.articles.length > 0) {
+                const i = Math.floor(Math.random() * res.data.articles.length);
+                const article = res.data.articles[i];
+                showMessage(
+                  newsMessage({
+                    title: article.title,
+                    description: article.description,
+                    articleUrl: article.url.startsWith("https")
+                      ? article.url
+                      : "#",
+                    imageUrl: article.image.startsWith("https")
+                      ? article.image
+                      : "#",
+                  }),
+                  20000
+                );
+              }
+            });
+        },
+      });
+
+      SpeechRecognizer.addCommand({
+        prompt: "how's the weather",
+        callback: () => {
+          getIpInfo()
+            .then((res) => {
+              const loc = res.data.loc.split(",");
+              return getWeatherOneCall(loc[0], loc[1]);
+            })
+            .then((res) => {
+              const curr = res.data.current;
+              const iconCode = curr.weather[0].icon;
+              const iconUrl = "https://openweathermap.org/img/wn/";
+              const current: CurrentWeather = {
+                temperature: Math.round(curr.temp),
+                feelsLike: Math.round(curr.feels_like),
+                weather: curr.weather[0].main,
+                icon: iconUrl + iconCode + "@2x.png",
+                code: iconCode,
+                class: "", // class isnt needed here
+              };
+              showMessage(weatherMessage(current), 10000);
+            });
+        },
+      });
+
       SpeechRecognizer.startRecognizer();
       return () => SpeechRecognizer.stopRecognizer();
     } else {
@@ -53,11 +113,73 @@ const Assistant = () => {
     }
   };
 
-  const showMessage = (msg: string) => {
+  const showMessage = (msg: string | JSX.Element, timeout: number) => {
     setMessage(msg);
     setTimeout(() => {
       setMessage("");
-    }, 5000);
+    }, timeout);
+  };
+
+  const newsMessage = (news: News) => {
+    let msgs = [
+      "This might interest you. Take a look.",
+      "Here's one I found. Check out the Home tab for more.",
+      "I saw this one in the headlines. What do you think?",
+      "Have a look at this one, isn't it interesting?",
+      "How about this piece? I was quite intrigued by it.",
+    ];
+    const msgIdx = Math.floor(Math.random() * 5);
+
+    return (
+      <div className={styles.newsMessage}>
+        <div>{msgs[msgIdx]}</div>
+        <div className={styles.newsContainer}>
+          <div className={styles.newsImgContainer}>
+            <img src={news.imageUrl} alt="" className={styles.newsImg} />
+          </div>
+          <a href={news.articleUrl} className={styles.newsTitle}>
+            {news.title}
+          </a>
+          <div className={styles.newsDescription}>
+            {news.description ? news.description : ""}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const weatherMessage = (currentWeather: CurrentWeather) => {
+    let msg = "Here's the latest weather forecast in your area: ";
+    if (currentWeather.feelsLike > 30) {
+      msg = "It's quite hot today.";
+    } else if (currentWeather.feelsLike > 15) {
+      msg = "The weather's really nice! Would you like to head out for a walk?";
+    } else if (currentWeather.feelsLike >= 0) {
+      msg =
+        "It's a bit chilly. Make sure to wear an extra layer if you're heading out.";
+    } else if (currentWeather.feelsLike < 0) {
+      msg = "It's cold outside. Would be nice to stay inside and stay cozy.";
+    }
+
+    return (
+      <div className={styles.weatherMessage}>
+        <div>{msg}</div>
+        <div className={styles.display}>
+          <div className={styles.icon}>
+            <img src={currentWeather.icon} alt="" />
+            <span>{currentWeather.weather}</span>
+          </div>
+          <div className={styles.temperature}>
+            <div>Temperature</div>
+            {currentWeather.temperature}°C
+          </div>
+          <div className={styles.feelsLike}>
+            <div>Feels Like</div>
+            {currentWeather.feelsLike}°C
+          </div>
+        </div>
+      </div>
+    );
   };
 
   return (
