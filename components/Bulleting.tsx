054d9138e8d8b16a910ca0cbe8f674770485f454
Ryanksa <61597505+Ryanksa@users.noreply.github.com>
2022-02-20 19:38:57 -0500

    Bulleting v2 (#17)

--- components/Bulleting.tsx
+++ components/Bulleting.tsx
@@ -1,17 +1,36 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, CSSProperties } from "react";
 import styles from "../styles/Bulleting.module.scss";
-
+import { getRandomArbitrary, getRandomInt } from "../utils/general";
+import { Directions, Bullet, Buff } from "../models/bulleting";
 import { Button } from "@mui/material";
-import { getRandomArbitrary } from "../utils/general";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FastForwardIcon from "@mui/icons-material/FastForward";
+import TimerIcon from "@mui/icons-material/Timer";
 
-import { Bullet } from "../models/bullet";
-import { Directions } from "../models/directions";
+const REFRESH_RATE = 10;
 
 // Game difficulty settings
-let charSpeed = 2;
-let charSize = 10;
-let bulletSpeed = 10;
+let charSpeed = 3;
+let charSize = 15;
+let iframeDuration = 500;
+let bulletSpeed = 3;
+let bulletSpeedScale = 0.2;
 let bulletSize = 10;
+let bulletSpawnInterval = 1000;
+let buffSize = 20;
+let buffSpawnInterval = 5000;
+
+const resettingDiffucltSettings = () => {
+  charSpeed = 3;
+  charSize = 15;
+  iframeDuration = 500;
+  bulletSpeed = 3;
+  bulletSpeedScale = 0.2;
+  bulletSize = 10;
+  bulletSpawnInterval = 1000;
+  buffSize = 20;
+  buffSpawnInterval = 5000;
+};
 
 // For controlling character
 const keyState = {
@@ -38,6 +57,12 @@ const directions: Directions = {
     else keyState.right = 0;
   },
 };
+const resetKeyState = () => {
+  keyState.up = 0;
+  keyState.down = 0;
+  keyState.left = 0;
+  keyState.right = 0;
+};
 
 // Collision logic
 const isColliding = (
@@ -55,12 +80,56 @@ const isColliding = (
     ((t2t1 >= 0 && t2t1 <= s1) || (t2t1 <= 0 && t2t1 >= -s2))
   );
 };
+let iframeOn = false;
+
+// Game buffs
+const buffs = {
+  health: 0,
+  speed: 1,
+  zaWarudo: 2,
+};
 
 export default function Bulleting() {
   // For game mechanics
   const containerRef = useRef<HTMLDivElement>(null);
   const charRef = useRef<HTMLDivElement>(null);
   const [bullets, setBullets] = useState<Bullet[]>([]);
+  const [lives, setLives] = useState(3);
+  const [buffs, setBuffs] = useState<Buff[]>([]);
+  const buffTypes = [
+    {
+      id: 0,
+      effect: () => setLives((lives) => lives + 1),
+      icon: <FavoriteIcon className={styles.lifeIcon} />,
+    },
+    {
+      id: 1,
+      effect: () => (charSpeed += 0.5),
+      icon: <FastForwardIcon className={styles.speedIcon} />,
+    },
+    {
+      id: 2,
+      effect: () => {
+        if (charRef.current && containerRef.current) {
+          bulletSpeed /= 4;
+          bulletSpeedScale /= 4;
+          const effect = document.createElement("div");
+          effect.classList.add(styles.zaWarudoEffect);
+          effect.style.setProperty("--left", charRef.current.offsetLeft + "px");
+          effect.style.setProperty("--top", charRef.current.offsetTop + "px");
+          containerRef.current.appendChild(effect);
+
+          setTimeout(() => {
+            containerRef.current?.removeChild(effect);
+            bulletSpeed *= 4;
+            bulletSpeedScale *= 4;
+          }, 5000);
+        }
+      },
+      icon: <TimerIcon className={styles.zaWarudoIcon} />,
+    },
+  ];
+
   // For game states
   const [gameEnded, setGameEnded] = useState(false);
   const [topScore, setTopScore] = useState(0);
@@ -77,6 +146,8 @@ export default function Bulleting() {
       const charH = container.offsetHeight - charSize;
       const bulletW = container.offsetWidth - bulletSize;
       const bulletH = container.offsetHeight - bulletSize;
+      const buffW = container.offsetWidth - buffSize;
+      const buffH = container.offsetHeight - buffSize;
 
       // For controlling character
       const keydownCallback = (e: KeyboardEvent) => {
@@ -90,7 +161,7 @@ export default function Bulleting() {
       document.addEventListener("keydown", keydownCallback);
       document.addEventListener("keyup", keyupCallback);
 
-      // Spawn a new bullet in every second
+      // Spawn a new bullet in every second and chance to speed up bullets
       let bulletSpawnerId = setInterval(() => {
         setBullets((bullets) => [
           ...bullets,
@@ -101,9 +172,25 @@ export default function Bulleting() {
             leftVelocity: getRandomArbitrary(-1, 1),
           },
         ]);
-      }, 1000);
+        bulletSpeed += bulletSpeedScale;
+      }, bulletSpawnInterval);
 
-      // Game loop, 10ms refresh rate
+      // Chance to spawn in a buff every 5s
+      let buffSpawnerId = setInterval(() => {
+        if (getRandomInt(0, 10) < 3) {
+          const buffId = getRandomInt(0, 3);
+          setBuffs((buffs) => [
+            ...buffs,
+            {
+              top: getRandomArbitrary(0, buffH),
+              left: getRandomArbitrary(0, buffW),
+              type: buffId,
+            },
+          ]);
+        }
+      }, buffSpawnInterval);
+
+      // Game loop
       let gameId = setInterval(() => {
         // Character movement
         if (keyState.up) {
@@ -117,7 +204,7 @@ export default function Bulleting() {
           char.style.left = `${Math.min(char.offsetLeft + charSpeed, charW)}px`;
         }
 
-        // Bullets movement
+        // Bullets movement and collision logic
         setBullets((bullets) => {
           const newBullets: Bullet[] = [];
           for (let i = 0; i < bullets.length; i++) {
@@ -144,6 +231,7 @@ export default function Bulleting() {
             }
 
             if (
+              !iframeOn &&
               isColliding(
                 char.offsetLeft,
                 char.offsetTop,
@@ -153,19 +241,54 @@ export default function Bulleting() {
                 bulletSize
               )
             ) {
-              exitGame();
+              setLives((lives) => {
+                if (lives > 1) {
+                  iframeOn = true;
+                  char.classList.add(styles.hit);
+                  setTimeout(() => {
+                    iframeOn = false;
+                    char.classList.remove(styles.hit);
+                  }, iframeDuration);
+                  return lives - 1;
+                } else {
+                  exitGame();
+                  return 0;
+                }
+              });
             }
 
             newBullets.push(newBullet);
           }
           return newBullets;
         });
-      }, 10);
+
+        // Buff collision logic
+        setBuffs((buffs) => {
+          for (let i = 0; i < buffs.length; i++) {
+            if (
+              isColliding(
+                char.offsetLeft,
+                char.offsetTop,
+                charSize,
+                buffs[i].left,
+                buffs[i].top,
+                buffSize
+              )
+            ) {
+              const buff = buffTypes[buffs[i].type];
+              if (buff) buff.effect();
+              return [...buffs.slice(0, i), ...buffs.slice(i + 1)];
+            }
+          }
+          return buffs;
+        });
+      }, REFRESH_RATE);
 
       // Exit game
       const exitGame = () => {
         clearInterval(gameId);
         clearInterval(bulletSpawnerId);
+        clearInterval(buffSpawnerId);
         document.removeEventListener("keydown", keydownCallback);
         document.removeEventListener("keyup", keyupCallback);
         resetKeyState();
@@ -176,6 +299,18 @@ export default function Bulleting() {
         exitGame();
       };
     }
+  };
+
+  const restartGame = () => {
+    if (charRef.current) {
+      charRef.current.style.left = `calc(50% - ${charSize / 2}px`;
+      charRef.current.style.top = `calc(50% - ${charSize / 2}px`;
+    }
+    setBullets([]);
+    setBuffs([]);
+    setLives(3);
+    resettingDiffucltSettings();
+    initGame();
   };
 
   useEffect(initGame, []);
@@ -197,37 +332,34 @@ export default function Bulleting() {
     }
   }, [gameEnded]);
 
-  const restartGame = () => {
-    if (charRef.current) {
-      charRef.current.style.left = `calc(50% - ${charSize / 2}px`;
-      charRef.current.style.top = `calc(50% - ${charSize / 2}px`;
-    }
-    setBullets([]);
-    initGame();
-  };
-
-  const resetKeyState = () => {
-    keyState.up = 0;
-    keyState.down = 0;
-    keyState.left = 0;
-    keyState.right = 0;
-  };
-
   return (
     <div>
-      <div className={styles.header}>
+      <div className={styles.stats}>
+        <div className={styles.lives}>
+          Lives:
+          {[...Array(lives)].map((_, idx) => (
+            <FavoriteIcon
+              key={idx}
+              className={styles.lifeIcon}
+              style={{ "--size": "20px" } as CSSProperties}
+            />
+          ))}
+        </div>
         <div className={styles.score}>
           Score: <span>{bullets.length}</span>
         </div>
-        <div className={styles.score + " " + styles.top}>
-          Best: <span>{topScore}</span>
-        </div>
       </div>
+
       <div className={styles.gameContainer} ref={containerRef}>
         <div
           className={styles.character}
           ref={charRef}
-          style={{ "--size": `${charSize}px` } as React.CSSProperties}
+          style={
+            {
+              "--size": `${charSize}px`,
+              "--iframe-duration": `${iframeDuration}ms`,
+            } as CSSProperties
+          }
         />
         {bullets.map((bullet, idx) => (
           <div
@@ -238,14 +370,34 @@ export default function Bulleting() {
                 "--size": `${bulletSize}px`,
                 "--top": `${bullet.top}px`,
                 "--left": `${bullet.left}px`,
-              } as React.CSSProperties
+              } as CSSProperties
             }
           />
+        ))}
+        {buffs.map((buff, idx) => (
+          <div
+            key={idx}
+            className={styles.buff}
+            style={
+              {
+                "--size": `${buffSize}px`,
+                "--top": `${buff.top}px`,
+                "--left": `${buff.left}px`,
+              } as CSSProperties
+            }
+          >
+            {buffTypes[buff.type].icon}
+          </div>
         ))}
 
         {gameEnded && (
           <div className={styles.gameOverContainer}>
-            <div className={styles.gameOverText}>Game Over...</div>
+            <div className={styles.gameOver}>
+              <div className={styles.gameOverText}>Game Over</div>
+              <div className={styles.topScore}>
+                Best Score: <span>{topScore}</span>
+              </div>
+            </div>
             <Button variant="contained" onClick={restartGame}>
               Restart
             </Button>
--- models/bullet.ts
+++
@@ -1,6 +0,0 @@
-export interface Bullet {
-  top: number;
-  left: number;
-  topVelocity: number;
-  leftVelocity: number;
-}
