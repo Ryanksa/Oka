3343731df84e58f1d8dcaffb51430e68484f5e9f
Ryanksa <ryanlin58@gmail.com>
2023-02-13 20:26:17 -0500

    Added new loading spinner animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,10 @@ import styles from "../styles/Loading.module.scss";
 const Loading = () => {
   return (
     <div className={styles.loadingContainer}>
-      <div className={styles.loading} />
+      <div className={styles.loading}>
+        <div className={styles.mass} />
+        <div className={styles.massMask} />
+      </div>
     </div>
   );
 };
