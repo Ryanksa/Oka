fe71b5873174327109f096b30c1b35dc3835e25a
Ryanksa <ryanlin58@gmail.com>
2024-06-02 15:44:00 -0400

    Allow fullscreen on TakeABreak page

--- pages/takeabreak/index.tsx
+++ pages/takeabreak/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import OkaHead from "../../src/components/OkaHead";
 import HotSpring from "../../src/components/HotSpring";
 import MountainOcean from "../../src/components/MountainOcean";
@@ -11,12 +12,32 @@ const TakeABreak = () => {
   const palette = takeABreak.hotSpringPalette;
   const topScore = takeABreak.bulletingTopScore;
 
+  const contentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const toggleFullscreen = (event: KeyboardEvent) => {
+      if (event.key.toLowerCase() !== "f") return;
+
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        contentRef.current?.requestFullscreen();
+      }
+    };
+
+    document.addEventListener("keydown", toggleFullscreen);
+    return () => {
+      document.removeEventListener("keydown", toggleFullscreen);
+    };
+  }, []);
+
   return (
     <>
       <OkaHead title="Take a Break" />
-      {option === BreakOption.hotspring && <HotSpring palette={palette} />}
-      {option === BreakOption.mountainocean && <MountainOcean />}
-      {option === BreakOption.bulleting && <Bulleting topScore={topScore} />}
+      <div ref={contentRef}>
+        {option === BreakOption.hotspring && <HotSpring palette={palette} />}
+        {option === BreakOption.mountainocean && <MountainOcean />}
+        {option === BreakOption.bulleting && <Bulleting topScore={topScore} />}
+      </div>
     </>
   );
 };
