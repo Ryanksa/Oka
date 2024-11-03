c0bc7723126d3f799b56510fdb3fc855421d49cd
Ryanksa <ryanlin58@gmail.com>
2024-11-03 10:44:08 -0500

    Update to use weather 3.0 api

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -7,7 +7,7 @@ const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
 export const getWeatherOneCall = (lat: string, lon: string) => {
   return fetch(
-    `${apiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${apiKey}`
+    `${apiUrl}/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${apiKey}`
   )
     .then((response) => {
       if (response.ok) return response.json();
@@ -21,7 +21,7 @@ export const getWeatherOneCall = (lat: string, lon: string) => {
 
 export const useWeatherOneCall = (lat: string, lon: string) => {
   const { data, error } = useSWR(
-    `${apiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${apiKey}`
+    `${apiUrl}/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${apiKey}`
   );
   return {
     weather: data,
